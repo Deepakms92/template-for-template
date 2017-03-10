0762b4a9979afea38293664dccbe05bccc0e5b2b
Deepakms92 <deepakms92@gmail.com>
2017-03-10 06:04:58 +0000

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 $(window).load(function() {
     $('.flexslider').flexslider({
-    animation: "fade",
+    animation: "slide",
     controlNav: false,
     directionNav: true,
     slideshowSpeed: 5000,
